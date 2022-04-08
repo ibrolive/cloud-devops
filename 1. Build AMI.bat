43f4cc892b9969ea652cf0eda13d3d2ab1676cc1
Ibrahim Dauda <ibrahim.dauda@dnata.com>
2022-04-08 17:09:45 +0400

    updated folder names

--- 1. Build AMI.bat
+++ 1. Build AMI.bat
@@ -1,5 +1,5 @@
 @echo off
-cd ./0.AMI
+cd ./_0.AMI
 set PACKER_LOG=10
 set PACKER_LOG_PATH=./packer.log
 echo starting AMI build
--- 2. Provision-Jenkins.bat
+++ 2. Provision-Jenkins.bat
@@ -1,4 +1,4 @@
-cd ./1. terraform-jenkins-server
+cd ./_1. terraform-jenkins-server
 terraform init -input=false
 terraform plan -out=tfplan -input=false -var-file=jenkins.tfvars
 terraform apply --auto-approve -input=false tfplan
