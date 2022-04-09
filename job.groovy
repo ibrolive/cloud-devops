e68ed9152d0273bd73af7894bfdf4fede4f59909
Ibrahim Dauda <ibrahim.dauda@dnata.com>
2022-04-09 11:15:28 +0400

    added dsl job

---
+++ job.groovy
@@ -0,0 +1,14 @@
+pipelineJob("CICD") {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url("https://github.com/ibrolive/cloud-devops.git")
+          }
+        }
+      }
+      scriptPath("3. ci-cd/CI-CD.Jenkinsfile")
+    }
+  }
+}
