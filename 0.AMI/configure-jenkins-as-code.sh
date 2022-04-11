8192966af40e56c1973e0c13447c8af5c5b05109
Ibrahim Dauda <ibrahim.dauda@dnata.com>
2022-04-11 12:41:13 +0400

    bug fix for jcasc config file location

--- 0.AMI/configure-jenkins-as-code.sh
+++ 0.AMI/configure-jenkins-as-code.sh
@@ -1,4 +1,4 @@
 # copy jcasc configuration to jenkins home directory
-cp ./jenkins.yaml /var/lib/jenkins/jenkins.yaml
+mv /tmp/jenkins.yaml /var/lib/jenkins/jenkins.yaml
 # add jenkins user to the docker group to allow jenkins to execute docker commands
 usermod -a -G docker jenkins
