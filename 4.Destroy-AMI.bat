1d195e0633128821b59f1704d6fc57acb70a35c2
Ibrahim Dauda <ibrahim.dauda@dnata.com>
2022-04-18 14:46:56 +0400

    updated tags

--- 4.Destroy-AMI.bat
+++ 4.Destroy-AMI.bat
@@ -2,13 +2,14 @@
 echo Destroying AMI...
 
 set REGION=us-east-1
-aws ec2 describe-images --filters "Name=name,Values=custom-jenkins-ami" --query "Images[*].[ImageId]" --output text --region %REGION% > temp
+set AWS_PROFILE=default
+aws ec2 describe-images --filters "Name=name,Values=jenkins-server" --query "Images[*].[ImageId]" --output text --region %REGION% > temp
 set /p TEMPVAR=<temp
 more temp
 del temp
 aws ec2 deregister-image --image-id %TEMPVAR% --region %REGION%
 
-aws ec2 describe-snapshots --filters "Name=tag:Environment,Values=devops-demo" --query "Snapshots[*].{ID:SnapshotId}" --output text --region %REGION% > temp
+aws ec2 describe-snapshots --filters "Name=tag:Environment,Values=jenkins-server" --query "Snapshots[*].{ID:SnapshotId}" --output text --region %REGION% > temp
 set /p TEMPVAR=<temp
 del temp
 aws ec2 delete-snapshot --snapshot-id %TEMPVAR% --region %REGION%
