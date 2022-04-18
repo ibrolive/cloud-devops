c9f2f15acfc70bf16081ee8054b2dbcaad7561f0
Ibrahim Dauda <ibrahim.dauda@dnata.com>
2022-04-18 15:13:07 +0400

    excluded elb dns url for jenkins server

--- 1. terraform-jenkins-server/outputs.tf
+++ 1. terraform-jenkins-server/outputs.tf
@@ -1,6 +1,6 @@
 output "Jenkins_URL" {
   value = "http://${aws_instance.custom-jenkins-ami.public_ip}:8080"
 }
-output "ELB_DNS_URL" {
-  value = "http://${aws_elb.jenkins_elb.dns_name}"
-}
+# output "ELB_DNS_URL" {
+#   value = "http://${aws_elb.jenkins_elb.dns_name}"
+# }
