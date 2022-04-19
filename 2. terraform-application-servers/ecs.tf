26e3eb6f6874cc77162aedd51e455ec4170633b6
Ibrahim Dauda <ibrahim.dauda@dnata.com>
2022-04-19 16:34:57 +0400

    added backend service

--- 2. terraform-application-servers/ecs.tf
+++ 2. terraform-application-servers/ecs.tf
@@ -80,3 +80,27 @@ resource "aws_ecs_service" "frontend" {
   depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
 }
 
+resource "aws_ecs_service" "backend" {
+  name            = "devops-service-backend"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.app_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = aws_subnet.private.*.id
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.app.id
+    container_name   = "devops-app-backend"
+    container_port   = var.app_port_backend
+  }
+
+  tags = local.tags
+
+  depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
+}
+
